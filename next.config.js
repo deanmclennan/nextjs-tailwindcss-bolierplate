f157c306c509e07f9e0da524cda9b355e0d613c9
satmstr <deanmclennan83@gmail.com>
2022-12-09 21:36:53 +1100

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  // allow url to be passed to next/image
+
+  images: {
+    domains: ["images.unsplash.com", "a.espncdn.com"],
+  },
+
+  formats: [
+    "image/avif",
+    "image/webp",
+    "image/jpeg",
+    "image/png",
+    "image/svg+xml",
+  ],
+  experimental: {
+    // Allows the use of new experimental features in Next.js
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
